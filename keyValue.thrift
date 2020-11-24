5c944768e013f1145f57f30c9de9a761166fd9cc
milindparlawar123 <50922528+milindparlawar123@users.noreply.github.com>
2020-11-24 16:51:54 -0500

    base code

---
+++ keyValue.thrift
@@ -0,0 +1,30 @@
+exception SystemException {
+  1: optional string message
+}
+
+enum ConsistencyLevel {
+	ONE;
+	QUORUM;
+}
+
+struct ReadOrWriteRequest {
+    1: required bool isCoordinator;
+	2: optional string timestamp;
+	3: required ConsistencyLevel consistencyLevel;
+}
+
+struct ReplicaID{
+	1: string id;
+	2: string ip;
+	3: i32 port;
+}
+service ReplicatedKeyValueStore {
+
+  
+  string get(1: Request request, 2: i32 key,  3: ReplicaID replicaID)
+    throws (1: SystemException systemException),
+
+  bool put(1: Request request, 2: i32 key, 3: string  value, 4: ReplicaID replicaID)
+    throws (1: SystemException systemException),
+
+}
